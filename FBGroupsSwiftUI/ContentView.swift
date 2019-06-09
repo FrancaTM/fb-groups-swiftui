a574e9eac9849210110ddfc890c53c6ff1ef78b9
FrancaTM <francatm@outlook.com>
2019-06-09 11:50:44 -0300

    Initial Commit

---
+++ FBGroupsSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FBGroupsSwiftUI
+//
+//  Created by Tulio Marcos Franca on 09/06/19.
+//  Copyright © 2019 Tulio Marcos Franca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("ok World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
