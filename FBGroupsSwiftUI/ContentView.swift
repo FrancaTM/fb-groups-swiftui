c90a1317132615ee51b34f972051c0ad92e507f1
FrancaTM <francatm@outlook.com>
2019-06-12 01:50:52 -0300

    add dynamic list view sample

--- FBGroupsSwiftUI/ContentView.swift
+++ FBGroupsSwiftUI/ContentView.swift
@@ -8,10 +8,12 @@
 
 import SwiftUI
 
+// MARK: Facebook complex layout
+
 struct Post {
     let id: Int
     let username, text, profileImageName, imageName: String
-    
+
     static let samplePosts: [Post] = [
         .init(id: 1, username: "Bill Clinton", text: "I swear I didn't touch that girl, she came on to me!", profileImageName: "burger", imageName: "post_puppy"),
         .init(id: 2, username: "Barack Obama", text: "I used to be the president that people claimed wasn't born in the United States, but that isn't true", profileImageName: "burger", imageName: "burger"),
@@ -23,7 +25,7 @@ struct Post {
 struct Group {
     let id: Int
     let name, imageName: String
-    
+
     static let sampleGroups: [Group] = [
         .init(id: 1, name: "Co-Ed Hikes of Colorado", imageName: "hike"),
         .init(id: 2, name: "Other People's Puppies", imageName: "puppy"),
@@ -36,32 +38,31 @@ struct Group {
     ]
 }
 
-struct ContentView : View {
+struct ContentView: View {
     let posts = Post.samplePosts
-    
+
     var body: some View {
         NavigationView {
             List {
-                VStack (alignment: .leading) {
+                VStack(alignment: .leading) {
                     Text("Trending").font(.headline).padding(.leading, 16).padding(.bottom, 4)
-                    ScrollView (showsHorizontalIndicator: false) {
+                    ScrollView(showsHorizontalIndicator: false) {
                         HStack {
                             ForEach(Group.sampleGroups.identified(by: \.imageName)) { group in
-                                
+
                                 NavigationButton(destination: GroupDetailView(group: group)) {
                                     GroupView(group: group).padding(.trailing, 8)
                                 }
                             }
-                            }.padding(.leading, 16).padding(.trailing, 8)
-                        }.frame(height: 180)
-                    }.padding(.top, 8).padding(.leading, -20).padding(.trailing, -20)
-                
+                        }.padding(.leading, 16).padding(.trailing, 8)
+                    }.frame(height: 180)
+                }.padding(.top, 8).padding(.leading, -20).padding(.trailing, -20)
+
                 ForEach(posts.identified(by: \.id)) { post in
                     PostView(post: post).padding(.bottom)
                 }
-                }.navigationBarTitle(Text("Groups"), displayMode: .automatic)
+            }.navigationBarTitle(Text("Groups"), displayMode: .automatic)
         }
-        
     }
 }
 
@@ -71,7 +72,7 @@ struct GroupDetailView: View {
         VStack {
             Image(group.imageName)
             Text(group.name)
-            }.navigationBarTitle(Text(group.name))
+        }.navigationBarTitle(Text(group.name))
     }
 }
 
@@ -92,28 +93,80 @@ struct GroupView: View {
 struct PostView: View {
     let post: Post
     var body: some View {
-        VStack (alignment: .leading, spacing: 16) {
+        VStack(alignment: .leading, spacing: 16) {
             HStack {
                 Image("burger").resizable().frame(width: 50, height: 50).clipShape(Circle()).clipped()
-                VStack (alignment: .leading) {
+                VStack(alignment: .leading) {
                     Text(post.username).font(.headline)
                     Text("Posted 8 hrs ago").font(.body)
-                    }.padding(.leading, 8)
-                }.padding(.leading, 16)
+                }.padding(.leading, 8)
+            }.padding(.leading, 16)
             Text(post.text).padding(.leading, 16).padding(.trailing, 36).lineLimit(nil)
-            
+
             Image(post.imageName, bundle: nil)
                 .scaledToFill()
                 .frame(height: 300)
                 .clipped()
-            }.padding(.leading, -20).padding(.trailing, -20).padding(.top, 12).padding(.bottom, -26)
+        }.padding(.leading, -20).padding(.trailing, -20).padding(.top, 12).padding(.bottom, -26)
     }
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+#endif
+
+// MARK: Dynamic List
+
+struct User: Identifiable {
+    var id: Int
+    let name, message, imageName: String
+}
+
+struct DynamicListView: View {
+    let users: [User] = [
+        .init(id: 0, name: "Tim Cook", message: "My stands cost $999", imageName: "tim_cook"),
+        .init(id: 1, name: "Craig Federighi", message: "I have the sexiest hair in the land of Apple spokesmen", imageName: "craig_f"),
+        .init(id: 2, name: "Jony Ive", message: "Somebody save me, I have no idea where I am anymore. Do I even work at Apple anymore? I bet you sure miss my sexy ass accent during the keynotes, amiright?", imageName: "jony_ive")
+    ]
+
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Users").font(.largeTitle)
+                ForEach(users.identified(by: \.id)) { user in
+                    UserRow(user: user)
+                }
+            }.navigationBarTitle(Text("Dynamic List"))
+        }
+    }
+}
+
+struct UserRow: View {
+    let user: User
+
+    var body: some View {
+        return HStack {
+            Image(user.imageName)
+                .resizable()
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                .frame(width: 70, height: 70).shadow(radius: 4)
+            VStack(alignment: .leading) {
+                Text(user.name).font(.headline)
+                Text(user.message).font(.subheadline).lineLimit(nil)
+            }.padding(.leading, 8)
+        }.padding(.init(top: 12, leading: 0, bottom: 12, trailing: 0))
+    }
+}
+
+#if DEBUG
+struct DynamicListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DynamicListView()
     }
 }
 #endif
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Project based on:
 
 [SwiftUI: Facebook Complex Layouts - Horizontal Scroll View ](https://www.youtube.com/watch?v=7QgPpvqTfeo)
 
+[SwiftUI Basics: Dynamic Lists, HStack VStack, Images with Circle Clipped Stroke Overlays](https://www.youtube.com/watch?v=bz6GTYaIQXU&t=18s)
